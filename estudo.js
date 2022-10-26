a81dda0714b096b3daafb0341b67ca8d19c7324d
sommelier de camisinha <108942314+Cartero17@users.noreply.github.com>
2022-10-26 11:08:28 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,41 +1,37 @@
-
 function moeda(atual){
 return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 }
-
 function total(){
   let c = document.getElementById("valor").value;
   let j = document.getElementById("juros").value;
   let t = document.getElementById("meses").value;
   let r = moeda;
-
-
   if(!Number(c)){
-      
    alert("O capital deve ser numeros.");
    document.getElementById("valor").value = "";
    document.getElementById("valor").focus();
    return;
   }
-
-  if(!Number(j)){   
+  if(!Number(j)){
     alert("O juros deve ser numeros.");
     document.getElementById("juros").value = "";
     document.getElementById("juros").focus();
     return;
    }
-
    if(!Number(t)){
     alert("O meses deve ser numeros.");
     document.getElementById("meses").value = "";
     document.getElementById("meses").focus();
     return;
    }
+     for(let i = 1; i <= t; i++){
+         r = c * (1 + (j/100));
+         mes += "Mês" + 1 + 1 " valor: " + moeda(r) + "<br>";
+         //document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
+         c = r;
   }
+  document.getElementById("mes").innerHTML=mes;
 
-  for(let i = 1; i <= t; i++){
-      r = c * (1 + (j/100));
-      document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
-      c = moeda(r);
+  document.getElementById("total").innerHTML="Total: "+moeda(r);
+  //document.write("Resultado: " + moeda(r));
   }
-  document.write("Resultado: " + moeda(r));
